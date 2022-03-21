23b00b2c9291cde0c37fb27a183896dc71a0cf19
Сергей Сивонкин <sivonkin.serg@yandex.ru>
2022-03-21 13:28:39 +0300

    step added

---
+++ s2.1step4.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import math
+
+try:
+    link = "http://suninjuly.github.io/math.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    def calc(x):
+        return str(math.log(abs(12*math.sin(int(x)))))
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    answer = browser.find_element_by_id("answer")
+    answer.send_keys(str(math.log(abs(12*math.sin(int(x))))))
+    option1 = browser.find_element_by_css_selector("[for='robotCheckbox']")
+    option1.click()
+    option2 = browser.find_element_by_css_selector("[for='robotsRule']")
+    option2.click()
+    option3 = browser.find_element_by_class_name("btn.btn-default")
+    option3.click()
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(5)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+    #
